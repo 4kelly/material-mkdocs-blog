3f0af689b7a3004f122fc03688410f5be912e22e
Ryan <user@4kelly.com>
2021-11-12 13:19:58 -0500

    cloudflare analytics

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,3 @@
+<!-- Cloudflare Web Analytics -->
+<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "038de8174d3f414bad78f3262ebcc6a8"}'></script>
+<!-- End Cloudflare Web Analytics -->
